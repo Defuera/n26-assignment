74906ed823427813764d9f18eef1aa2ca5cde603
defuera <vdenis@zvooq.com>
2017-03-26 16:34:29 +0200

    configure libraries

---
+++ app/src/main/java/ru/justd/n26assignment/MainPresenter.kt
@@ -0,0 +1,13 @@
+package ru.justd.n26assignment
+
+import ru.justd.arkitec.presenter.BasePresenter
+
+/**
+ * Created by defuera on 26/03/2017.
+ */
+class MainPresenter : BasePresenter<MainView>() {
+
+    override fun onViewAttached() {
+
+    }
+}
---
+++ app/src/main/java/ru/justd/n26assignment/MainView.kt
@@ -0,0 +1,7 @@
+package ru.justd.n26assignment
+
+/**
+ * Created by defuera on 26/03/2017.
+ */
+interface MainView {
+}
